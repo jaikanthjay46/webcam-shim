b958a3872041cd276b122403d71d405c551ed037
Jaikanth J <jaikanthjay46@gmail.com>
2020-09-13 11:11:49 +0530

    version: prettier

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,28 @@
 function mediaDeviceWrapper(constraints: MediaStreamConstraints, cb: any, err: any) {
-    navigator.mediaDevices.getUserMedia(constraints)
-        .then( (e: any) => cb(e))
-        .catch( (e: any) => err(e));
+  navigator.mediaDevices
+    .getUserMedia(constraints)
+    .then((e: any) => cb(e))
+    .catch((e: any) => err(e));
 }
 
 export function getUserMedia(constraints: MediaStreamConstraints, cb: any, err: any) {
-    navigator.getUserMedia = (navigator as any).getUserMedia ||
-                 (navigator as any).webkitGetUserMedia ||
-                 (navigator as any).mozGetUserMedia 
+  navigator.getUserMedia =
+    (navigator as any).getUserMedia || (navigator as any).webkitGetUserMedia || (navigator as any).mozGetUserMedia;
 
-    if ( typeof navigator.mediaDevices.getUserMedia === "function") {
-        navigator.getUserMedia = navigator.getUserMedia || mediaDeviceWrapper;
-    }
-    if (navigator.getUserMedia) {
-        navigator.getUserMedia(constraints,
-            (stream) => {
-                cb(stream);
-            },
-            (e) => {
-                err(e);
-            }
-        );
-    } else {
-        err("Device not supports");
-    }
+  if (typeof navigator.mediaDevices.getUserMedia === 'function') {
+    navigator.getUserMedia = navigator.getUserMedia || mediaDeviceWrapper;
+  }
+  if (navigator.getUserMedia) {
+    navigator.getUserMedia(
+      constraints,
+      (stream) => {
+        cb(stream);
+      },
+      (e) => {
+        err(e);
+      },
+    );
+  } else {
+    err('Device not supports');
+  }
 }
