98af0fe4d4b6f38087a8c198cb0d003f917ac39a
Jaikanth J <jaikanthjay46@gmail.com>
2020-09-13 10:40:41 +0530

    add: write shim

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+function mediaDeviceWrapper(constraints: MediaStreamConstraints, cb: any, err: any) {
+    navigator.mediaDevices.getUserMedia(constraints)
+        .then( (e: any) => cb(e))
+        .catch( (e: any) => err(e));
+}
+
+export function getUserMedia(constraints: MediaStreamConstraints, cb: any, err: any) {
+    navigator.getUserMedia = (navigator as any).getUserMedia ||
+                 (navigator as any).webkitGetUserMedia ||
+                 (navigator as any).mozGetUserMedia 
+
+    if ( typeof navigator.mediaDevices.getUserMedia === "function") {
+        navigator.getUserMedia = navigator.getUserMedia || mediaDeviceWrapper;
+    }
+    if (navigator.getUserMedia) {
+        navigator.getUserMedia(constraints,
+            (stream) => {
+                cb(stream);
+            },
+            (e) => {
+                err(e);
+            }
+        );
+    } else {
+        err("Device not supports");
+    }
+}
